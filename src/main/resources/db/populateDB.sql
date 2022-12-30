fcd5a1d7c8adfb0d8c90f5b83a2dd915760757e0
MrMcLaine <Lypovskyi@gmail.com>
2022-12-30 17:40:51 +0200

    add spring-app & spring-db

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -17,26 +17,26 @@ VALUES ('USER', 100000),
        ('USER', 100002),
        ('USER', 100003);
 
-INSERT INTO meals (restaurant_id, description, price, date)
-VALUES (100013,'Marine Soil (-20 M)', 30, now()),
-    (100013,'Low Andes Mountain (1800 M)', 44, now()),
-    (100013,'Extreme Stem (2875 M)', 26, now()),
-    (100014,'gazpacho with "snow"', 28, now()),
-    (100014,'Jewels of ‘White Pearl’ oysters with Corsican mint and Vodka-tonic gels', 77, now()),
-    (100014,'Salted Pollack from Yeu Island with pine salt and fermented cabbage', 34, now()),
-    (100015, 'Daisy Flower', 33, now()),
-    (100015, 'Love to Eat Avocado Toast', 68, now()),
-    (100015, 'Shining star', 49, now());
-
 INSERT INTO restaurants (name, menu)
 VALUES ('Central, Lima', 'empty menu'),
        ('Bon-Bon, Brussels', 'empty menu'),
        ('Atelier Crenn, San Francisco', 'empty menu');
 
+INSERT INTO meals (restaurant_id, description, price, date)
+VALUES (100004,'Marine Soil (-20 M)', 30, now()),
+       (100004,'Low Andes Mountain (1800 M)', 44, now()),
+       (100004,'Extreme Stem (2875 M)', 26, now()),
+       (100005,'gazpacho with "snow"', 28, now()),
+       (100005,'Jewels of ‘White Pearl’ oysters with Corsican mint and Vodka-tonic gels', 77, now()),
+       (100005,'Salted Pollack from Yeu Island with pine salt and fermented cabbage', 34, now()),
+       (100006, 'Daisy Flower', 33, now()),
+       (100006, 'Love to Eat Avocado Toast', 68, now()),
+       (100006, 'Shining star', 49, now());
+
 INSERT INTO votes(user_id, date, restaurant_id)
-VALUES (100000, now(), 100013),
-       (100001, now(), 100013),
-       (100002, now(), 100014),
-       (100003, now(), 100015);
+VALUES (100000, now(), 100004),
+       (100001, now(), 100005),
+       (100002, now(), 100005),
+       (100003, now(), 100006);
 
 
