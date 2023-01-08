088f65d9b823f91c861a45f56f749ded055cfe6b
MrMcLaine <Lypovskyi@gmail.com>
2023-01-08 17:37:35 +0200

    add RestaurantProfileRestControllerTest

---
+++ src/test/java/org/voting/web/controller/user/RestaurantProfileRestControllerTest.java
@@ -0,0 +1,48 @@
+package org.voting.web.controller.user;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.voting.web.AbstractControllerTest;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.voting.RestaurantTestData.*;
+import static org.voting.RestaurantTestData.atelierCrenn;
+import static org.voting.TestUtil.userHttpBasic;
+import static org.voting.UserTestData.USER;
+
+public class RestaurantProfileRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = RestaurantProfileRestController.REST_URL + "/";
+
+    @Test
+    public void getAll() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL)
+                .with(userHttpBasic(USER)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(RESTAURANT_MATCHER.contentJson(central, bonBon, atelierCrenn));
+    }
+
+    @Test
+    public void getAllWithMeals() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "?withMenu=true")
+                .with(userHttpBasic(USER)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(RESTAURANT_MATCHER.contentJson(central, bonBon, atelierCrenn));
+    }
+
+    @Test
+    public void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + CENTRAL_ID)
+                .with(userHttpBasic(USER)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                // https://jira.spring.io/browse/SPR-14472
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(RESTAURANT_MATCHER.contentJson(central));
+    }
+}
