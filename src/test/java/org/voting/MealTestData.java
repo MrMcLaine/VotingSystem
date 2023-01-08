1dc567012ffac844aaf45d73cda1c9f1fb627e75
MrMcLaine <Lypovskyi@gmail.com>
2023-01-08 18:43:29 +0200

    add MealRestControllerTest & fill MealTestData

--- src/test/java/org/voting/MealTestData.java
+++ src/test/java/org/voting/MealTestData.java
@@ -2,35 +2,55 @@ package org.voting;
 
 import org.voting.entity.Meal;
 
-import java.time.Month;
 import java.util.List;
 
 import static org.voting.entity.abstractEntity.AbstractBaseEntity.START_SEQ;
 
 public class MealTestData {
-    public static final MatcherFactory.Matcher<Meal> MEAL_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(Meal.class, "user");
-/*
+    public static final MatcherFactory.Matcher<Meal> MEAL_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(Meal.class, "restaurant");
+
     public static final int NOT_FOUND = 10;
-    public static final int MEAL1_ID = START_SEQ + 4;
-    public static final int ADMIN_MEAL_ID = START_SEQ + 10;
+    public static final int MEAL1_CENTRAL_ID = START_SEQ + 7;
+    public static final int MEAL1_BONBON_ID = START_SEQ + 10;
+    public static final int MEAL1_ATELIERCRENN_ID = START_SEQ + 13;
 
-    public static final Meal meal1 = new Meal(MEAL1_ID, of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500);
-    public static final Meal meal2 = new Meal(MEAL1_ID + 1, of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000);
-    public static final Meal meal3 = new Meal(MEAL1_ID + 2, of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500);
-    public static final Meal meal4 = new Meal(MEAL1_ID + 3, of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100);
-    public static final Meal meal5 = new Meal(MEAL1_ID + 4, of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 500);
-    public static final Meal meal6 = new Meal(MEAL1_ID + 5, of(2020, Month.JANUARY, 31, 13, 0), "Обед", 1000);
-    public static final Meal meal7 = new Meal(MEAL1_ID + 6, of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 510);
-    public static final Meal adminMeal1 = new Meal(ADMIN_MEAL_ID, of(2020, Month.JANUARY, 31, 14, 0), "Админ ланч", 510);
-    public static final Meal adminMeal2 = new Meal(ADMIN_MEAL_ID + 1, of(2020, Month.JANUARY, 31, 21, 0), "Админ ужин", 1500);
+    public static final Meal mealCentral1 = new Meal(MEAL1_CENTRAL_ID,
+            "Marine Soil (-20 M)", 30);
+    public static final Meal mealCentral2 = new Meal(MEAL1_CENTRAL_ID + 1,
+            "Low Andes Mountain (1800 M)", 44);
+    public static final Meal mealCentral3 = new Meal(MEAL1_CENTRAL_ID + 2,
+            "Extreme Stem (2875 M)", 26);
 
-    public static final List<Meal> meals = List.of(meal7, meal6, meal5, meal4, meal3, meal2, meal1);
+    public static final Meal mealBonBon1 = new Meal(MEAL1_BONBON_ID,
+            "gazpacho with \"snow\"", 28);
+    public static final Meal mealBonBon2 = new Meal(MEAL1_BONBON_ID + 1,
+            "Jewels of ‘White Pearl’ oysters with Corsican mint and Vodka-tonic gels", 77);
+    public static final Meal mealBonBon3 = new Meal(MEAL1_BONBON_ID + 2,
+            "Salted Pollack from Yeu Island with pine salt and fermented cabbage", 34);
+
+    public static final Meal mealAtelierCrenn1 = new Meal(MEAL1_ATELIERCRENN_ID,
+            "Daisy Flower", 33);
+    public static final Meal mealAtelierCrenn2 = new Meal(MEAL1_ATELIERCRENN_ID + 1,
+            "Love to Eat Avocado Toast", 68);
+    public static final Meal mealAtelierCrenn3 = new Meal(MEAL1_ATELIERCRENN_ID + 2,
+            "Shining star", 49);
+    public static final Meal mealAtelierCrenn4 = new Meal(MEAL1_ATELIERCRENN_ID + 3,
+            "Some test meal in past", 1);
+    public static final Meal mealAtelierCrenn5 = new Meal(MEAL1_ATELIERCRENN_ID + 4,
+            "Second test meal in past", 1);
+
+    public static final List<Meal> mealsCentralForToday = List.of(mealCentral3, mealCentral2, mealCentral1);
+
+    public static final List<Meal> mealsAllDates = List.of(mealAtelierCrenn5, mealAtelierCrenn4, mealAtelierCrenn3,
+            mealAtelierCrenn2, mealAtelierCrenn1, mealBonBon3, mealBonBon2,
+            mealBonBon1, mealCentral3, mealCentral2, mealCentral1);
 
     public static Meal getNew() {
-        return new Meal(null, of(2020, Month.FEBRUARY, 1, 18, 0), "Созданный ужин", 300);
+        return new Meal(null, "New Meal", 2);
     }
 
     public static Meal getUpdated() {
-        return new Meal(MEAL1_ID, meal1.getDateTime().plusMinutes(2), "Обновленный завтрак", 200);
-    }*/
+        return new Meal(MEAL1_CENTRAL_ID, mealCentral1.getDateMeal().plusDays(2),
+                "Updated Meal", 31);
+    }
 }
---
+++ src/test/java/org/voting/web/controller/admin/MealRestControllerTest.java
@@ -0,0 +1,108 @@
+package org.voting.web.controller.admin;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.voting.entity.Meal;
+import org.voting.service.MealService;
+import org.voting.util.exception.NotFoundException;
+import org.voting.web.AbstractControllerTest;
+import org.voting.web.json.JsonUtil;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.voting.MealTestData.*;
+import static org.voting.MealTestData.NOT_FOUND;
+import static org.voting.RestaurantTestData.CENTRAL_ID;
+import static org.voting.TestUtil.userHttpBasic;
+import static org.voting.UserTestData.ADMIN;
+import static org.voting.UserTestData.USER;
+
+
+public class MealRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = MealRestController.REST_URL + "/";
+
+    @Autowired
+    private MealService service;
+
+    @Test
+    public void createWithLocations() throws Exception {
+        Meal newMeal = getNew();
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL, CENTRAL_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN))
+                .content(JsonUtil.writeValue(newMeal)))
+                .andExpect(status().isCreated());
+
+        Meal created = MEAL_MATCHER.readFromJson(action);
+        int newId = created.id();
+        newMeal.setId(newId);
+        MEAL_MATCHER.assertMatch(created, newMeal);
+        MEAL_MATCHER.assertMatch(service.getActualMenu(CENTRAL_ID), mealCentral1, mealCentral2, mealCentral3, created);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + MEAL1_CENTRAL_ID, CENTRAL_ID)
+                .with(userHttpBasic(ADMIN)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        assertThrows(NotFoundException.class, () -> service.get(MEAL1_CENTRAL_ID, CENTRAL_ID));
+    }
+
+    @Test
+    public void update() throws Exception {
+        Meal updated = getUpdated();
+        perform(MockMvcRequestBuilders.put(REST_URL + MEAL1_CENTRAL_ID, CENTRAL_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN))
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isNoContent());
+        MEAL_MATCHER.assertMatch(service.get(MEAL1_CENTRAL_ID, CENTRAL_ID), updated);
+    }
+
+    @Test
+    public void getActualMenu() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + CENTRAL_ID)
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isOk());
+        MEAL_MATCHER.assertMatch(service.getActualMenu(CENTRAL_ID), mealsCentralForToday);
+    }
+
+    @Test
+    public void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + CENTRAL_ID, MEAL1_CENTRAL_ID)
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                // https://jira.spring.io/browse/SPR-14472
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MEAL_MATCHER.contentJson(mealCentral1));
+    }
+
+    @Test
+    public void getUnAuth() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL, CENTRAL_ID))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void getForbidden() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL, CENTRAL_ID)
+                .with(userHttpBasic(USER)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void getNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + NOT_FOUND)
+                .with(userHttpBasic(ADMIN)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+}
