66f036a7cd801a3eaa9637676af5a1079682533f
MrMcLaine <Lypovskyi@gmail.com>
2023-01-15 02:26:36 +0200

    Also clean code and to add spring-cache.xml files

--- src/test/java/org/voting/TestUtil.java
+++ src/test/java/org/voting/TestUtil.java
@@ -1,35 +1,12 @@
 package org.voting;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.voting.entity.person.User;
-import org.voting.web.json.JsonUtil;
-
-import java.io.UnsupportedEncodingException;
 
 public class TestUtil {
-
-/*    public static void mockAuthorize(User user) {
-        SecurityContextHolder.getContext().setAuthentication(
-                new UsernamePasswordAuthenticationToken(new AuthorizedUser(user), null, user.getRoles()));
-    }*/
 
     public static RequestPostProcessor userHttpBasic(User user) {
         return SecurityMockMvcRequestPostProcessors.httpBasic(user.getEmail(), user.getPassword());
     }
-
-/*    public static RequestPostProcessor userAuth(User user) {
-        return SecurityMockMvcRequestPostProcessors.authentication(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
-    }*/
-
-/*    public static <T> T readFromJsonMvcResult(MvcResult result, Class<T> clazz) throws UnsupportedEncodingException {
-        return JsonUtil.readValue(getContent(result), clazz);
-    }*/
-
-/*    public static String getContent(MvcResult result) throws UnsupportedEncodingException {
-        return result.getResponse().getContentAsString();
-    }*/
 }
